e9e89e3edf339ab8f32cc71d0ad76191cc967c3c
Connor Brake <153332456+ConnorBrake@users.noreply.github.com>
2023-12-21 11:07:51 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/testreview2/TestReview2.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.testreview2;
+
+
+public class TestReview2 {
+
+    public static void main(String[] args) {
+        System.out.println(Review.extremeFakeReview("SimpleReview.txt"));
+    }
+    
+}
